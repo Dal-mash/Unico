3200b93a3744e0769f340ab1c332b8daadd66020
Dalmash <rehmantaha77@gmail.com>
2026-02-07 14:44:42 +0500

    added animted bg, fixed carasoul, optimization

--- frontend/src/components/CTA.jsx
+++ frontend/src/components/CTA.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
+import Footer from "./Footer";
 
 export default function CTA() {
   return (
-    <section id="contact" className="min-h-screen h-screen flex items-center justify-center bg-white text-slate-900 snap-start">
-      <div className="text-center px-6 lg:px-20">
-        <h2 className="text-5xl font-extrabold text-blue-900">Ready to light your space?</h2>
-        <p className="mt-4 text-slate-600 max-w-2xl mx-auto">Contact us for a site review and tailored proposal. We focus on outcomes that last.</p>
+    <section
+      id="contact"
+      className="min-h-screen h-screen flex justify-center  items-center flex-col bg-white text-slate-900 snap-start"
+    >
+      <div className="text-center mt-auto  px-6 lg:px-20">
+        <h2 className="text-5xl font-extrabold text-blue-900">
+          Ready to light your space?
+        </h2>
+        <p className="mt-4 text-slate-600 max-w-2xl mx-auto">
+          Contact us for a site review and tailored proposal. We focus on
+          outcomes that last.
+        </p>
         <div className="mt-8">
-          <button className="px-8 py-4 bg-blue-900 text-white rounded-md text-lg font-semibold">Get started</button>
+          <button className="px-8 py-4 hover:cursor-pointer bg-blue-900 text-white rounded-md text-lg font-semibold">
+            Get started
+          </button>
         </div>
       </div>
+      <Footer />
     </section>
-  )
+  );
 }
